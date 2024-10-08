77ff52029582246defa87a9c0db65ac5f95fbb1f
Paulo Henrique <paulo.h.soares@iesb.edu.br>
2024-10-07 21:53:42 -0300

    atividade07.10

--- src/math.js
+++ src/math.js
@@ -1,11 +1,47 @@
-function fatorial() {
-
+function fatorial(n) {
+  n = Number(n);
+  if (typeof n !== 'number' || isNaN(n)) {
+      throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n < 0) {
+      throw new Error("Não existe fatorial de número negativo");
+  }
+  if (n === 0) {
+      return 1;
+  }
+  return n * fatorial(n - 1);
 }
-function fibonacci() {
 
+function fibonacci(n) {
+  n = Number(n);
+  if (typeof n !== 'number') {
+    throw new TypeError("O argumento deve ser um número válido");
 }
-function ehPrimo() {
-
+if (n <= 0) {
+    throw new Error("Não existe fibonnacci de número menor ou igual a zero");
+}
+if (n === 1) {
+    return 1;
+}
+if (n === 2) {
+    return 1;
+}
+return fibonacci(n - 1) + fibonacci(n - 2);
+}
+function ehPrimo(n) {
+  n = Number(n);
+  if (typeof n !== 'number' || isNaN(n)) {
+      throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 1) {
+      throw new Error("Não número primo de número menor ou igual a 1");
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+          return false;
+      }
+  }
+  return true;
 }
 
 module.exports = {
