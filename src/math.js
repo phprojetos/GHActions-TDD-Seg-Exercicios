f25dc660a8107d044e76ca9d6badbcb788bb745e
phprojetos <paulo.h.soares@iesb.edu.br>
2024-10-13 11:00:31 -0300

    Implementação das funções em Math.js

--- src/math.js
+++ src/math.js
@@ -1,51 +1,43 @@
+// src/Math.js
+
 function fatorial(n) {
-  n = Number(n);
-  if (typeof n !== 'number' || isNaN(n)) {
-      throw new TypeError("O argumento deve ser um número válido");
-  }
-  if (n < 0) {
-      throw new Error("Não existe fatorial de número negativo");
-  }
-  if (n === 0) {
-      return 1;
-  }
-  return n * fatorial(n - 1);
+    if (typeof n !== 'number') {
+        throw new TypeError('O argumento deve ser um número válido');
+    }
+    if (n < 0) {
+        throw new Error('Não existe fatorial de número negativo');
+    }
+    return n === 0 ? 1 : n * fatorial(n - 1);
 }
 
 function fibonacci(n) {
-  n = Number(n);
-  if (typeof n !== 'number') {
-    throw new TypeError("O argumento deve ser um número válido");
+    if (typeof n !== 'number') {
+        throw new TypeError('O argumento deve ser um número válido');
+    }
+    if (n <= 0) {
+        throw new Error('Não existe fibonacci de número menor ou igual a zero');
+    }
+    if (n === 1 || n === 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
-if (n <= 0) {
-    throw new Error("Não existe fibonnacci de número menor ou igual a zero");
-}
-if (n === 1) {
-    return 1;
-}
-if (n === 2) {
-    return 1;
-}
-return fibonacci(n - 1) + fibonacci(n - 2);
-}
+
 function ehPrimo(n) {
-  n = Number(n);
-  if (typeof n !== 'number' || isNaN(n)) {
-      throw new TypeError("O argumento deve ser um número válido");
-  }
-  if (n <= 1) {
-      throw new Error("Não número primo de número menor ou igual a 1");
-  }
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) {
-          return false;
-      }
-  }
-  return true;
+    if (typeof n !== 'number') {
+        throw new TypeError('O argumento deve ser um número válido');
+    }
+    if (n <= 1) {
+        throw new Error('Não existe número primo de número menor ou igual a 1');
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 module.exports = {
-  fatorial,
-  fibonacci,
-  ehPrimo
-}
+    fatorial,
+    fibonacci,
+    ehPrimo
+};
